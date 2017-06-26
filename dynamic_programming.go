a70100dd19e0b4c72d334ce64cd5afcccac76295
Ethan Brooks <ethanabrooks@gmail.com>
2017-06-26 10:05:22 -0700

    fixed up hashCode and reformatted

--- dynamic_programming.go
+++ dynamic_programming.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gonum/matrix/mat64"
+	"golang.org/x/sync/syncmap"
 	"math"
 	"math/rand"
-	"golang.org/x/sync/syncmap"
 )
 
 func randNormVector(size int) *mat64.Vector {
@@ -34,8 +34,8 @@ func simpleRandomWalk(steps int) *mat64.Dense {
 			vel -= acc
 		}
 		//vel *= noise           // vel *= noise
-		oldPos := int(positions.At(i - 1, 0))
-		positions.Set(i, 0, float64(oldPos + vel))
+		oldPos := int(positions.At(i-1, 0))
+		positions.Set(i, 0, float64(oldPos+vel))
 	}
 	return positions
 }
@@ -56,22 +56,19 @@ func randomWalk(steps int, dim int) *mat64.Dense {
 	return positions
 }
 
-
-func hashCode(path *mat64.Dense, start int, stop int, nChoices ...int) string {
-	_, dim := path.Dims()
-	return fmt.Sprint(nChoices, mat64.Formatted(path.Slice(start, stop, 0, dim).T()))
+func hashCode(start int, stop int, nChoices ...int) string {
+	return fmt.Sprint(start, stop, nChoices)
 }
 
-type CacheValue struct{
+type CacheValue struct {
 	choices []int
-	cost float64
+	cost    float64
 }
 
 type Cache syncmap.Map
 
-
 func getCost(path *mat64.Dense, start int, stop int, cache *syncmap.Map) float64 {
-	key := hashCode(path, start, stop)
+	key := hashCode(start, stop)
 
 	// check if return value has been cached
 	value, ok := cache.Load(key)
@@ -82,13 +79,13 @@ func getCost(path *mat64.Dense, start int, stop int, cache *syncmap.Map) float64
 	// check that stop and start haven't gotten goofed up
 	size, dim := path.Dims()
 	if start < 0 || stop > size {
-		panic(fmt.Sprintf( "start (%d) must be positive and stop (%d) must be less " +
+		panic(fmt.Sprintf("start (%d) must be positive and stop (%d) must be less "+
 			"than path length (%d)", start, stop, size))
 	}
 
 	// accumulate cost of path
 	cost := 0.0
-	for i := start + 1; i < stop; i ++ {
+	for i := start + 1; i < stop; i++ {
 		diffsSq := 0.0
 		for j := 0; j < dim; j++ {
 			diff := path.At(i, j) - path.At(start, j)
@@ -100,18 +97,17 @@ func getCost(path *mat64.Dense, start int, stop int, cache *syncmap.Map) float64
 	return cost
 }
 
-
 func _bestChoice(nChoices int, path *mat64.Dense, start int, stop int,
-cache *syncmap.Map) CacheValue {
+	cache *syncmap.Map) CacheValue {
 
-	key := hashCode(path, start, stop, nChoices)
-	
+	key := hashCode(start, stop, nChoices)
+
 	// check if return value has been cached
 	value, ok := cache.Load(key)
 	if ok {
 		return value.(CacheValue)
 	}
-	
+
 	// if not cached, calculate
 	if nChoices == 0 { // running out of choices is terminal condition
 		return CacheValue{nil, getCost(path, start, stop, cache)}
@@ -121,20 +117,21 @@ cache *syncmap.Map) CacheValue {
 	minCost := math.Inf(1)
 	var bestChoices []int
 
-	sem := make(chan bool, stop - start)
-	// TODO: parallel
+	sem := make(chan bool, stop-start)
 	for i := start + 1; i < stop; i++ {
-		go func (i int) {
-			sliceAfter := _bestChoice(nChoices - 1, path, i, stop, cache)
+		go func(i int) {
+			sliceAfter := _bestChoice(nChoices-1, path, i, stop, cache)
 			cost := getCost(path, start, i, cache) + sliceAfter.cost
 			if cost < minCost {
-				minCost =cost
+				minCost = cost
 				bestChoices = append(sliceAfter.choices, i)
 			}
 			sem <- true
 		}(i)
 	}
-	for i := start + 1; i < stop; i++ { <-sem }
+	for i := start + 1; i < stop; i++ {
+		<-sem
+	}
 	value = CacheValue{bestChoices, minCost}
 	cache.Store(key, value) // add return value to cache
 	return value.(CacheValue)
